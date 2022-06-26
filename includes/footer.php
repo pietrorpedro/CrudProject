05694d7e8d41c901e6080076fc9a13c1b3353431
Pietro Rodrigues Pedro <pietrorpedro@gmail.com>
2022-06-26 20:51:27 -0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <footer class="text-center" >
     <h5>Made by Pietro Pedro</h5>
-    <a href="https://github.com/pietrorpedro"><p>github.com/pietrorpedro</p></a>
+    <a href="https://github.com/pietrorpedro" target="__blank"><p>github.com/pietrorpedro</p></a>
 </footer>
